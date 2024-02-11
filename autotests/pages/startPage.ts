48295ecb7e44a98d0e9f15e505a06cb8b787a0d9
Christine-Pinto <chrissie2788@gmail.com>
2024-02-11 23:34:30 +0100

    syntax cleanup

--- autotests/pages/startPage.ts
+++ autotests/pages/startPage.ts
@@ -1,4 +1,4 @@
-import { expect, type Locator, type Page} from '@playwright/test';
+import { expect, type Locator, type Page } from '@playwright/test';
 
 export class StartPage {
   //readonly page: Page;
@@ -7,15 +7,16 @@ export class StartPage {
   readonly allupperButtons: Locator;
   readonly manageTokenButton: Locator;
   readonly allTokensNames: Locator;
-  
 
   constructor(readonly page: Page) {
-   // this.page = page;
+    // this.page = page;
     //expect(page.url()).toContain("options.html");
     this.balance = page.locator('div.BalanceContainer-sc-1ks78fh > p');
     this.tokens = page.locator('h1.CoinBalanceText-sc-wr88cs');
     this.allupperButtons = page.locator('div.RowButtonContainer-sc-1cgcddu > div');
-    this.manageTokenButton = page.locator('button, input[type="button"] >> text="Manage token list"');
+    this.manageTokenButton = page.locator(
+      'button, input[type="button"] >> text="Manage token list"',
+    );
     this.allTokensNames = page.locator('h1.SubText-sc-18704il');
   }
 
@@ -25,20 +26,24 @@ export class StartPage {
     await expect(this.balance).toBeVisible();
     await expect(this.manageTokenButton).toBeVisible();
     // Check if all 4 buttons (send, receive, swap, buy) are visible
-    // TODO : create more stable selector for all 4 buttons to be able to check all 4 indivudially
+    // TODO : create more stable selector for all 4 buttons to be able to check all 4 individually
     await expect(this.allupperButtons).toHaveCount(4);
   }
 
   async getBalancefloat(): Promise<number> {
-       //Check balance
-       const Balance = await this.balance.textContent();
-       const cleaned_balance = Balance.replace(/[^\d.-]/g, '');
-       const balanceFloat = parseFloat(cleaned_balance);
-       return balanceFloat
+    const Balance = await this.balance.textContent();
+    if (Balance !== null) {
+      const cleaned_balance = Balance.replace(/[^\d.-]/g, '');
+      const balanceFloat = parseFloat(cleaned_balance);
+      return balanceFloat;
+    } else {
+      // Handle the case where Balance is null
+      console.error('Failed to retrieve balance text content');
+      return 0;
+    }
   }
 
   async getBalancefloat_fromAllTokens(): Promise<number> {
-     // Get the count of elements matching the locator
     const count = await this.tokens.count();
     // Initialize a variable to hold the sum of all amounts
     let totalSum = 0;
@@ -46,20 +51,20 @@ export class StartPage {
     for (let i = 0; i < count; i++) {
       // Get the amount from the text
       const balanceToken = await this.tokens.nth(i).textContent();
+      if (balanceToken !== null) {
+        // Use a regular expression to remove non-numeric characters except the decimal point as the format is "$ 0.00 USD"
+        const cleanedText = balanceToken.replace(/[^\d.-]/g, '');
+        const amountFloat = parseFloat(cleanedText);
 
-      // Use a regular expression to remove non-numeric characters except the decimal point as the format is "$ 0.00 USD"
-      const cleanedText = balanceToken.replace(/[^\d.-]/g, '');
-      
-      // Parse the cleaned text as a float
-      const amountFloat = parseFloat(cleanedText);
-
-      // Check if the parsed value is a valid number before adding to the total sum
-      if (!isNaN(amountFloat)) {
-        totalSum += amountFloat;
+        // Check if the parsed value is a valid number before adding to the total sum
+        if (!isNaN(amountFloat)) {
+          totalSum += amountFloat;
+        }
+      } else {
+        // Handle the case where Balance is null
+        console.error('Failed to retrieve balance text content');
       }
     }
-    return totalSum
-}
-
-
+    return totalSum;
+  }
 }
--- autotests/tests/manageTokens.spec.ts
+++ autotests/tests/manageTokens.spec.ts
@@ -1,14 +1,13 @@
 import { expect, test } from '../fixtures/base';
 import { data } from '../fixtures/data';
 import Onboarding from '../pages/onboarding';
-import {StartPage} from '../pages/startPage';
-import {Token} from '../pages/token';
+import { StartPage } from '../pages/startPage';
+import { Token } from '../pages/token';
 
 //Main Issue with this testcase is that it depends currently on the successfull creation of a wallet (code for that was copied from onboarding.spec.ts)
 // TODO add either a mock for a created wallet or put creating of a wallet into a pageObject
 test.describe('Manage Tokens', () => {
   let onboarding: Onboarding;
-  //let startPage: StartPage;
   test.beforeEach(async ({ page, extensionId }) => {
     await page.goto(`chrome-extension://${extensionId}/popup.html`);
   });
@@ -46,52 +45,49 @@ test.describe('Manage Tokens', () => {
     await page.goto(`chrome-extension://${extensionId}/options.html`);
     let startPage = new StartPage(page);
     // Variable to check the changed balance after enable and disable a token
-    let balanceFloat = await startPage.getBalancefloat();;
-    let  balanceTokensFloat = await startPage.getBalancefloat_fromAllTokens();
+    let balanceFloat = await startPage.getBalancefloat();
+    let balanceTokensFloat = await startPage.getBalancefloat_fromAllTokens();
     await startPage.checkVisuals();
     await test.step('manage Tokens', async () => {
       await startPage.manageTokenButton.click();
       let token = new Token(page);
-      await token.checkVisuals(0,13);
+      await token.checkVisuals(0, 13);
 
-      await test.step('enable a random Tokens', async () => { 
+      await test.step('enable a random Tokens', async () => {
         // TODO add a  check if unselected tokens are avaible to enable a token better
         const enabled_token_name = await token.enableARandomToken();
         await token.backButton.click();
         await startPage.checkVisuals();
-        await expect (startPage.allTokensNames.getByText(enabled_token_name)).toBeVisible();
-      
-        const newBalanceTokensFloat = await startPage.getBalancefloat_fromAllTokens(); 
+        await expect(startPage.allTokensNames.getByText(enabled_token_name)).toBeVisible();
+
+        const newBalanceTokensFloat = await startPage.getBalancefloat_fromAllTokens();
         const newBalanceFloat = await startPage.getBalancefloat();
         await expect(newBalanceFloat).toEqual(newBalanceTokensFloat);
-        const differenceInBalance = balanceTokensFloat + newBalanceTokensFloat
+        const differenceInBalance = balanceTokensFloat + newBalanceTokensFloat;
         await expect(newBalanceFloat).toEqual(balanceFloat + differenceInBalance);
       });
 
-
-      await test.step('disable a random Tokens', async () => { 
+      await test.step('disable a random Tokens', async () => {
         // when switching from startpage to manage token the app had sometimes the problem with the status change and didn't show the enabled button, with adding this addiotnoial visual check the test got more stable be adding one more check
         // TODO figure out why app doesn't show enabled token in the fast switch --> potential bug !!!!
         // TODO change the check as the number only works if that was the frist token to be added
-        await expect (startPage.allTokensNames).toHaveCount(3);
-        // disable a token
+        await expect(startPage.allTokensNames).toHaveCount(3);
         await startPage.manageTokenButton.click();
-        await token.checkVisuals(1,12);
+        await token.checkVisuals(1, 12);
 
         const disabled_token_name = await token.disableARandomToken();
         await token.backButton.click();
         await expect(startPage.balance).toBeVisible();
-        await expect (startPage.allTokensNames.getByText(disabled_token_name)).toBeHidden();
+        await expect(startPage.allTokensNames.getByText(disabled_token_name)).toBeHidden();
 
         // Check if the new Balance is correct and compare with the original values
-        const newBalanceTokensFloat = await startPage.getBalancefloat_fromAllTokens(); 
+        const newBalanceTokensFloat = await startPage.getBalancefloat_fromAllTokens();
         const newBalanceFloat = await startPage.getBalancefloat();
 
         await expect(newBalanceFloat).toEqual(newBalanceTokensFloat);
-        const differenceInBalance = balanceTokensFloat - newBalanceTokensFloat
+        const differenceInBalance = balanceTokensFloat - newBalanceTokensFloat;
         await expect(newBalanceFloat).toEqual(balanceFloat - differenceInBalance);
       });
-
     });
   });
 });
